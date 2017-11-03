79673999ac832db4d389656c51145ce5f85c0c53
Colin Worf <colin.worf@gmail.com>
2017-11-03 14:30:11 -0700

    create image arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-function PizzaBuild (size, cheese, vegies, meat){
+function PizzaBuild (size, cheese, vegies, meats){
   this.size = size;
   this.cheese = cheese;
   this.vegies = vegies;
-  this.meat = meat;
+  this.meats = meats;
+  this.cheeseImg = getCheese(cheese);
+  this.vegiesImgs = getImages(this.vegies);
+  this.meatsImgs = getImages(meats);
+}
+
+function getCheese(cheese){
+  if (cheese && cheese !== "noCheese") {
+    return cheese + ".png";
+  }
+}
+function getImages(toppings){
+  console.log(toppings);
+  var imgArr = [];
+  toppings.forEach(function(topping){
+    imgArr.push(topping + ".png");
+    console.log(topping);
+  });
+  return imgArr;
 }
 var basePriceSet = {
   "small": 9,
@@ -17,15 +35,17 @@ var cheesePriceSet = {
 };
 
 PizzaBuild.prototype.price = function(){
-  var basePrice = basePriceSet[this.size];
-  var cheesePrice = cheesePriceSet[this.cheese];
-  var vegiesPrice = this.vegies.length;
-  var meatPrice = this.meat.length * 2.5;
+  var basePrice = basePriceSet[this.size],
+      cheesePrice = cheesePriceSet[this.cheese],
+      vegiesPrice = this.vegies.length,
+      meatPrice = this.meats.length * 2.5;
 
   return basePrice + cheesePrice + vegiesPrice + meatPrice;
 }
 
+PizzaBuild.prototype.addToCart = function(){
 
+}
 
 
 
@@ -35,18 +55,16 @@ $(function(){
     var selectedSize = $('input[name=size]:checked').val();
     var selectedCheese = $('input[name=cheese]:checked').val();
     var selectedVegies = [];
-    var selectedMeat = [];
-    var thisPizza = new PizzaBuild(selectedSize, selectedCheese, selectedVegies, selectedMeat);
+    var selectedMeats = [];
+    var thisPizza = new PizzaBuild(selectedSize, selectedCheese, selectedVegies, selectedMeats);
     var thisPrice = thisPizza.price()
 
     $('input[name=vegies]:checked').each(function(){
       selectedVegies.push($(this).val());
     });
     $('input[name=meats]:checked').each(function(){
-      selectedMeat.push($(this).val());
+      selectedMeats.push($(this).val());
     });
-
     console.log(thisPizza);
-    console.log(thisPrice);
   });
 });
