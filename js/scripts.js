bff316fd5f096a7b46aba00f4ee62e98d9fc7daf
Colin Worf <colin.worf@gmail.com>
2017-11-03 11:59:33 -0700

    create pizza builder object and input selected data

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
-function PizzaBuild (size, vegies, meat){
+function PizzaBuild (size, cheese, vegies, meat){
   this.size = size;
+  this.cheese = cheese;
   this.vegies = vegies;
   this.meat = meat;
-}
+};
+var basePrice = {
+  small: 9,
+  medium: 11,
+  large: 13,
+  extraLarge : 15,
+};
+var cheesePrice = {
+  noCheese: 0,
+  regCheese: 2,
+  xCheese: 3,
+};
+
+
+
+
+
+$(function(){
+  $('#pizza-builder').submit(function(event){
+    event.preventDefault();
+    var selectedSize = $('input[name=size]:checked').val();
+    var selectedCheese = $('input[name=cheese]:checked').val();
+    var selectedVegies = [];
+    var selectedMeat = [];
+
+    $('input[name=vegies]:checked').each(function(){
+      selectedVegies.push($(this).val());
+    });
+    $('input[name=meats]:checked').each(function(){
+      selectedMeat.push($(this).val());
+    });
+
+    var thisPizza = new PizzaBuild(selectedSize, selectedCheese, selectedVegies, selectedMeat);
+    console.log(thisPizza);
+  });
+});
