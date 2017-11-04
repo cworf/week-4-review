30654d3af6dbc98dcd4f374bc67222260713817b
Colin Worf <colin.worf@gmail.com>
2017-11-03 18:07:20 -0700

    add price to cart items

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,87 @@
-function Pizza (size, cheese, vegies, meats){
+var cartTotal = 0;
+var cartNumber = 0;
+function Pizza (size, cheese, veggies, meats){
   this.size = size;
   this.cheese = cheese;
-  this.vegies = vegies;
+  this.veggies = veggies;
   this.meats = meats;
   this.cheeseImg = getImages(cheese);
-  this.vegiesImgs = getImages(vegies);
+  this.veggiesImgs = getImages(veggies);
   this.meatsImgs = getImages(meats);
 }
 
 Pizza.prototype.price = function(){
   var basePrice = basePriceSet[this.size],
       cheesePrice = cheesePriceSet[this.cheese],
-      vegiesPrice = this.vegies.length,
+      veggiesPrice = this.veggies.length,
       meatPrice = this.meats.length * 2.5;
 
-  return basePrice + cheesePrice + vegiesPrice + meatPrice;
+  return basePrice + cheesePrice + veggiesPrice + meatPrice;
 }
 
-Pizza.prototype.CartItem = function(){
+Pizza.prototype.cartItem = function(){
+  cartTotal += this.price();
+  cartNumber++;
   return `
-  <div class='card'>
+  <div class='cart-item'>
     <div class='pizza-header'>
-      <img src='img/crust.png'>
-      <div class='topping-group' id='cheese-img'>`+ this.cheeseImg + `</div>
-      <div class='topping-group' id='cheese-img'>`+ this.meatsImgs.join("") + `</div>
-      <div class='topping-group' id='cheese-img'>`+ this.vegiesImgs.join("") + `</div>
+      <img src='img/crust.png'>` + this.cheeseImg +
+      this.meatsImgs.join("") +
+      this.veggiesImgs.join("") + `
+    </div>
+    <div class="pizza-price">
+      $` + this.price() + `
+    </div>
+    <div class="pizza-info">
+      <div id="accordion" role="tablist">
+        <div class="card">
+          <div class="card-header" role="tab" id="cheeseHeading">
+            <h5 class="mb-0">
+              <a data-toggle="collapse" href="#collapseCheese">
+                Cheese Selection
+              </a>
+            </h5>
+          </div>
+
+          <div id="collapseCheese" class="collapse show" role="tabpanel" data-parent="#accordion">
+            <div class="card-body">
+              ` + this.cheese + `
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" role="tab" id="meatHeading">
+            <h5 class="mb-0">
+              <a class="collapsed" data-toggle="collapse" href="#collapseMeat">
+                Meat Selections
+              </a>
+            </h5>
+          </div>
+          <div id="collapseMeat" class="collapse" role="tabpanel" data-parent="#accordion">
+            <div class="card-body">
+              <ul>
+                <li>` + this.meats.join("</li><li>") + `</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" role="tab" id="veggieHeading">
+            <h5 class="mb-0">
+              <a class="collapsed" data-toggle="collapse" href="#collapseVeggie">
+                Veggie Selections
+              </a>
+            </h5>
+          </div>
+          <div id="collapseVeggie" class="collapse" role="tabpanel" data-parent="#accordion">
+            <div class="card-body">
+              <ul>
+                <li>` + this.veggies.join("</li><li>") + `</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>`
 }
@@ -54,20 +111,20 @@ function getImages(toppings){
 }
 
 $(function(){
-  $('#pizza-builder').submit(function(event){
-    event.preventDefault();
+  $('#pizza-builder').change(function(){
+    // event.preventDefault();
     var selectedSize = $('input[name=size]:checked').val();
     var selectedCheese = $('input[name=cheese]:checked').val();
-    var selectedVegies = [];
+    var selectedVeggies = [];
     var selectedMeats = [];
 
-    $('input[name=vegies]:checked').each(function(){
-      selectedVegies.push($(this).val());
+    $('input[name=veggies]:checked').each(function(){
+      selectedVeggies.push($(this).val());
     });
     $('input[name=meats]:checked').each(function(){
       selectedMeats.push($(this).val());
     });
-    var thisPizza = new Pizza(selectedSize, selectedCheese, selectedVegies, selectedMeats);
+    var thisPizza = new Pizza(selectedSize, selectedCheese, selectedVeggies, selectedMeats);
     var thisPrice = thisPizza.price();
     console.log(thisPizza);
   });
